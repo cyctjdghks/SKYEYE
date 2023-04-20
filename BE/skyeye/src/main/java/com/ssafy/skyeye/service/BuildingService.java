5de8521769da1475e55a5a7d103139212c30fb71
박성환 <cyctjdghks@naver.com>
2023-04-20 10:25:34 +0900

    feat: be Create ServiceImpl

--- BE/skyeye/src/main/java/com/ssafy/skyeye/service/BuildingService.java
+++ BE/skyeye/src/main/java/com/ssafy/skyeye/service/BuildingService.java
@@ -1,4 +1,16 @@
 package com.ssafy.skyeye.service;
 
+import com.ssafy.skyeye.data.dto.request.BuildingRegistDto;
+import com.ssafy.skyeye.data.dto.request.BuildingUpdateDto;
+import com.ssafy.skyeye.data.dto.response.BuildingDto;
+import com.ssafy.skyeye.data.dto.response.CrackDto;
+
+import java.util.List;
+
 public interface BuildingService {
+    void registBuilding(BuildingRegistDto buildingRegistDto);
+    void updateBuilding(BuildingUpdateDto buildingUpdateDto);
+    void deleteBuilding(String buildingId);
+    List<CrackDto> getBuildingByCrackId(String buildingId);
+    BuildingDto getBuilding(String buildingId);
 }
--- BE/skyeye/src/main/java/com/ssafy/skyeye/service/impl/CrackServiceImpl.java
+++ BE/skyeye/src/main/java/com/ssafy/skyeye/service/impl/CrackServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ssafy.skyeye.service.impl;
 
+import com.ssafy.skyeye.data.dto.request.CrackRegistDto;
+import com.ssafy.skyeye.data.dto.request.CrackUpdateDto;
+import com.ssafy.skyeye.data.dto.response.CrackDto;
 import com.ssafy.skyeye.repository.CrackRepository;
 import com.ssafy.skyeye.service.CrackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +15,24 @@ public class CrackServiceImpl implements CrackService {
     private final CrackRepository crackRepository;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void registCrack(MultipartFile multipartFile, CrackRegistDto crackRegistDto) {
+
+    }
+
+    @Override
+    public CrackDto getCrack(String crackId) {
+        return null;
+    }
+
+    @Override
+    public void updateCrack(CrackUpdateDto crackUpdateDto) {
+
+    }
+
+    @Override
+    public void deleteCrack(String crackId) {
+
+    }
 }
