29676407793380c6cec3f5538a66bca14f1467c3
BingBang <byoung1997s4@gmail.com>
2023-04-26 14:10:38 +0900

    feat:fe main

--- FE/src/present/pages/main/Main.tsx
+++ FE/src/present/pages/main/Main.tsx
@@ -2,13 +2,9 @@ import * as style from "@pages/main/Main.style";
 import * as first from "@component/main/firstPage.style"
 import logo from "@assets/main/logo.png";
 import right from "@assets/main/right.png";
-import skyImg from "@assets/main/skyImg.jpg"
+import backVideo from "@assets/main/coverVideo.mp4"
 
 const Main = () => {
-
-  const scrollToBottm = () =>{
-
-  }
 
   return (
     <style.mainWrapper>
@@ -20,14 +16,15 @@ const Main = () => {
         </style.loginButton>
       </style.topBox>
       <first.firstPage>
-        <first.skyImg src={skyImg}></first.skyImg>
+        <first.backVideo muted autoPlay loop src={backVideo}></first.backVideo>
         <first.mainTextFirst>건물안전,</first.mainTextFirst><br/>
-        <first.mainTextSecond>SKYEYE</first.mainTextSecond>
+        <first.mainTextSecond>SKYEYE
         <first.mainTextThird>로 완성합니다.</first.mainTextThird>
+        </first.mainTextSecond>
       </first.firstPage>
-      {/* <style.secondPage>
+      <style.secondPage>
         
-      </style.secondPage> */}
+      </style.secondPage>
     </style.mainWrapper>
   );
 };
