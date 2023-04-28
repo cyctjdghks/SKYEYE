21a329509e445d72b3942dd8e363dae7249f5a59
BingBang <byoung1997s4@gmail.com>
2023-04-28 14:02:59 +0900

    fix: fe login

--- FE/src/present/component/main/modalcontent.tsx
+++ FE/src/present/component/main/modalcontent.tsx
@@ -1,17 +1,13 @@
 import InputLabel from "@common/InputLabel/InputLabel";
 import { DataInput } from "@src/action/hooks/Effectiveness";
 import * as style from "@component/main/modalContent.style";
-import { Login } from "@action/hooks/authHooks";
+import { UserLogin, DroneLogin } from "@action/hooks/authHooks";
 
 const ModalContent = () => {
   const [id, setId, idError] = DataInput(/^[a-zA-z0-9]{5,20}$/);
   const [pwd, setPwd, pwdError] = DataInput(
     /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{9,16}$/
   );
-
-  const clickLogin = () => {
-    Login(id, pwd);
-  };
 
   return (
     <style.loginModalWrap>
@@ -37,7 +33,7 @@ const ModalContent = () => {
             : "영어,숫자 특수문자를 하나이상 입력해주세요( 9~16 글자 )"
         }
       ></InputLabel>
-      <style.loginButton onClick={clickLogin}>로그인하기</style.loginButton>
+      <style.loginButton>로그인하기</style.loginButton>
     </style.loginModalWrap>
   );
 };
