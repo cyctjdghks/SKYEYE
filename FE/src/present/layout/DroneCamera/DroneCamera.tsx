e870cbcf0ca5b4f90f146f272988490cc9277869
C12H22O12 <director20844@gmail.com>
2023-05-01 13:37:22 +0900

    feat: prevent reload

--- FE/src/present/layout/DroneCamera/DroneCamera.tsx
+++ FE/src/present/layout/DroneCamera/DroneCamera.tsx
@@ -3,20 +3,38 @@ import PrimaryButton from "@src/present/common/Button/PrimaryButton";
 import * as Style from "./DroneCamera.style";
 
 const DroneCamera = () => {
+  // Prevent Go back
   const preventGoBack = () => {
     history.pushState(null, "", location.href);
     alert("종료하기 버튼을 눌러주세요");
+  };
+
+  // Prevent Reload
+  const preventClose = (e: BeforeUnloadEvent) => {
+    /*
+    Blocked attempt to show a 'beforeunload' confirmation panel for a frame that never had a user gesture since its load
+
+    -> 사용자가 페이지와 아무런 상호작용을 하지 않았을 때 발생하는 오류
+    한 번 영상 심어보고 그래도 뜨면 처리할게요
+    */
+
+    e.preventDefault();
+    e.returnValue = "";
   };
 
   useEffect(() => {
     (() => {
       history.pushState(null, "", location.href);
       window.addEventListener("popstate", preventGoBack);
+
+      window.addEventListener("beforeunload", preventClose);
     })();
 
     return () => {
       window.removeEventListener("popstate", preventGoBack);
-    }
+
+      window.removeEventListener("beforeunload", preventClose);
+    };
   }, []);
 
   return (
