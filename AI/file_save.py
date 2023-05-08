b6b0860633851daed70068dcbc98f1cf4ea6b29f
강모현 <rkdahgus12@naver.com>
2023-05-08 15:20:41 +0900

    feat:flask add

--- AI/file_save.py
+++ AI/file_save.py
@@ -30,4 +30,4 @@ def uploaded_file(filename):
     return 'Uploaded file: {}'.format(filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
