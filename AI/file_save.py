f27529ac0ca6b8d3d441ec3116c4acd450f2a7f8
강모현 <rkdahgus12@naver.com>
2023-05-09 16:36:58 +0900

    feat: high size uploading

--- AI/file_save.py
+++ AI/file_save.py
@@ -1,33 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Blueprint
 import os
 import datetime
-app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'  # 업로드 된 파일을 저장할 폴더 경로
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+bp = Blueprint('main', __name__, url_prefix='/api')
 
-@app.route('/AI')
+@bp.route('/ai')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@bp.route('/upload', methods=['POST'])
 def upload():
     # 업로드된 파일 가져오기
-    file = request.files['file']
+    files = request.files.getlist('files')
+    print(request.files)
 
     # 파일 저장 경로 설정
-    now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    filename = f"{now}.PNG"
-
-    # 파일 저장
-    file.save(filename)
-
+    
+    filenames = []
+    print('filename: ',filenames)
+    for file in request.files.getlist('file'):
+        now = datetime.datetime.now().strftime('{}{}{}{}{}{}'.format('%Y','%m','%d','%H','%M','%S%f'))
+        filename = f"images/{now}.jpg"
+        print('filename1: ',file)
+        file.save(filename)
+        filenames.append(filename)
+    print(filenames)
     # 파일 업로드 성공 메시지 반환
-    return redirect(url_for('uploaded_file', filename=file.filename))
+    return redirect(url_for('main.uploaded_file', filenames=filenames))
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return 'Uploaded file: {}'.format(filename)
+
+@bp.route('/uploads/cf')
+def uploaded_file():
+    return 'Uploaded file Upload Success'
+
+
+
+app = Flask(__name__)
+app.register_blueprint(bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
