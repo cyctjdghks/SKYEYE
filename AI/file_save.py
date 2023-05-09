0010056e24f384b7b45d84c08f56965e36bea72e
강모현 <rkdahgus12@naver.com>
2023-05-09 17:34:10 +0900

    feat:model fit

--- AI/file_save.py
+++ AI/file_save.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 import os
 import datetime
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
 
 bp = Blueprint('main', __name__, url_prefix='/api')
+######################## Tensorflow Classifier model Loading
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+
+class_names = open("labels.txt", "r").readlines()
+
+
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
 
 @bp.route('/ai')
 def index():
@@ -24,6 +39,30 @@ def upload():
         print('filename1: ',file)
         file.save(filename)
         filenames.append(filename)
+        image = Image.open(f"images/{now}.jpg").convert("RGB")
+
+
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+
+        image_array = np.asarray(image)
+
+
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+
+        data[0] = normalized_image_array
+
+
+        prediction = model.predict(data)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", confidence_score)
     print(filenames)
     # 파일 업로드 성공 메시지 반환
     return redirect(url_for('main.uploaded_file', filenames=filenames))
