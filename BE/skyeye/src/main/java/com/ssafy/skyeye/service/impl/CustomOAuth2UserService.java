4c968f4ce327a750be14ef49c4fa50a994061506
박재현 <jhp1276@naver.com>
2023-05-15 17:06:28 +0900

    Update CustomOAuth2UserService.java

--- BE/skyeye/src/main/java/com/ssafy/skyeye/service/impl/CustomOAuth2UserService.java
+++ BE/skyeye/src/main/java/com/ssafy/skyeye/service/impl/CustomOAuth2UserService.java
@@ -69,7 +69,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     public void registUser(String email, String src, String name, String provider){
         
-        log.info("{}, {}, {}, {} ", email, src, name, provider)
+        log.info("{}, {}, {}, {} ", email, src, name, provider);
 
         if(userRepository.existsById(email)) {
             User temp = userRepository.findById(email)
