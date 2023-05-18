ff8c341dfc1371c480f4cd02c6ed0db000f1140a
BingBang <byoung1997s4@gmail.com>
2023-05-18 12:44:47 +0900

    feat: fe router

--- FE/src/router/PrivateRouter.tsx
+++ FE/src/router/PrivateRouter.tsx
@@ -1,24 +1,10 @@
-import { useRecoilValue, useRecoilState } from "recoil";
-import { toastListState } from "@src/store/toast";
+import { useRecoilValue } from "recoil";
 import { authState } from "@src/store/auth";
 import { Outlet, Navigate } from "react-router-dom";
-import { useEffect } from "react";
 
 const PrivateRoute = () => {
   const isLogin = useRecoilValue(authState).isAuthenticated
   
-  const [toastList, setToastList] = useRecoilState(toastListState);
-
-  useEffect(()=>{
-    if(!isLogin){
-      const accessAdminToast = {
-        type: "Error",
-        sentence: "권한이 없습니다.",
-      };
-      setToastList({ list: [...toastList.list, accessAdminToast] });
-    }
-  }, [])
- 
   return !isLogin? <Navigate to="/" /> : <Outlet />;
 };
 
